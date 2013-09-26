f335350af4cc10344fde70da988a72129fb1b86b
tag_fujita <takashi_fujita@waku-2.com>
2013-09-26 12:12:28 +0900

    web

--- web.rb
+++ web.rb
@@ -1,18 +1,19 @@
 # coding: utf-8
 
-require "sinatra"
-require "json"
+require 'sinatra'
+require 'json'
 
 get '/' do
-  "Here is tagpandabot01"
+  'Here is tagpandabot01'
 end
 
 post '/' do
   content_type :text
   json = JSON.parse(request.body.read)
-  json["events"].map{ |e|
-    if e["message"]
-      "#{e["message"]["text"]}!, by #{e["message"]["nickname"]}!"
+  json['events'].select { |e| e['message'] }.map { |e|
+    text = e['message']['text']
+    if e['message']
+      text + '!'
     end
-  }.compact.join("\n")+"\n"
+  }.compact.join('\n') + '\n'
 end
