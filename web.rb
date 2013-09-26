ec077de2061916c4c37f73f1d4d7660bfa2de46d
tag_fujita <takashi_fujita@waku-2.com>
2013-09-26 12:00:14 +0900

    web

--- web.rb
+++ web.rb
@@ -3,9 +3,14 @@
 require "sinatra"
 require "json"
 
-post "/hi" do
-  j = JSON.parse(request.body.string)
-  j["events"].map{ |e|
+get '/' do
+  "Here is tagpandabot01"
+end
+
+post '/' do
+  content_type :text
+  json = JSON.parse(request.body.read)
+  json["events"].map{ |e|
     if e["message"]
       "Hi, #{e["message"]["nickname"]}!"
     end
