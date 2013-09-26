1e245ce2c6b494ee099e742e03bc755d4ee42800
tag_fujita <takashi_fujita@waku-2.com>
2013-09-26 15:39:34 +0900

    musicBot

--- web.rb
+++ web.rb
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 require 'sinatra'
+require 'net/http'
 require 'json'
 
 get '/' do
@@ -13,9 +14,23 @@ post '/' do
   json['events'].select { |e| e['message'] }.map { |e|
     text = e['message']['text']
     if e['message']
-      if /^!tagpanda/ =~ text
+      if /^!bestsong/ =~ text
         text =  text.strip.split(/[\s　]/)
-        'tagpanda says: ' + text[1] + '!'
+        artist = text[1]
+
+        url = 'http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&format=json&artist='
+        apikey = '&api_key=8b8d4214382f1f7c31873276e3a60d39'
+        limit = '&limit=1'
+
+        uri = URI.parse(url + artist + apikey + limit)
+        json = Net::HTTP.get(uri)
+        result = JSON.parse(json)
+
+        songname = result['toptracks']['track']['name']
+        imageL = result['toptracks']['track']['image'][3]['#text']
+
+        message = 'the best song of #{artist} is #{songname}!' + '\n'
+        message + imageL
       end
     end
   }.join
